1e3bf7d7a468ae3d075ddfcdd238b2e7a6d49a1c
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-02-04 16:14:49 +0900

    Feat: Convertor 테스트 코드 추가

--- src/test/java/racingcar/util/ConvertorTest.java
+++ src/test/java/racingcar/util/ConvertorTest.java
@@ -1,7 +1,27 @@
 package racingcar.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ConvertorTest {
 
+    @Test
+    void 입력된_정수만큼_미리_설정된_특수문자로_변환한다() {
+        // given
+        int one = 0;
+        int five = 5;
+        int ten = 10;
+
+        // when
+        String oneLine = Convertor.intToLine(one);
+        String fiveLine = Convertor.intToLine(five);
+        String tenLine = Convertor.intToLine(ten);
+
+        // then
+        assertThat(oneLine).hasSize(one);
+        assertThat(fiveLine).hasSize(five);
+        assertThat(tenLine).hasSize(ten);
+    }
 }
